067beff30078c36f394e918f365676d58b9f24fe
Augusto Medeiros <124903314+medeiros-augusto@users.noreply.github.com>
2023-08-30 18:01:03 -0300

    remove tabuada antiga

--- tabuada/script.js
+++ tabuada/script.js
@@ -5,6 +5,9 @@ function gerar(){
     if(num.value.length == 0){
         alert("[ ERRO ] Digite um número!")
     }else{
+        while(tabuada.firstChild){
+            tabuada.removeChild(tabuada.firstChild)
+        }
         for(var i = 1; i <= 10; i++){
             var opcao = document.createElement('option')
             opcao.innerText=`${num.value} x ${i} = ${Number(num.value) * i}`
