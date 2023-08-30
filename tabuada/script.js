b71af71e2340a289e12bf7f476695edcf4072804
Augusto Medeiros <124903314+medeiros-augusto@users.noreply.github.com>
2023-08-30 17:52:43 -0300

    Create script.js

---
+++ tabuada/script.js
@@ -0,0 +1,14 @@
+function gerar(){
+    var num = document.querySelector("#num")
+    var tabuada = document.querySelector("#tabuada")
+
+    if(num.value.length == 0){
+        alert("[ ERRO ] Digite um número!")
+    }else{
+        for(var i = 1; i <= 10; i++){
+            var opcao = document.createElement('option')
+            opcao.innerText=`${num.value} x ${i} = ${Number(num.value) * i}`
+            tabuada.appendChild(opcao)
+        }
+    }
+}
